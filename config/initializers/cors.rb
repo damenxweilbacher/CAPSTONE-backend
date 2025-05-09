979f7ecf698b8e79049412e8056eb94b8d5657b9
damenxweilbacher <weilbachers74@gmail.com>
2025-05-09 19:34:57 +1000

    changed link for cors due to wrong link being posted/ cors not allowing any actions or routes to go through.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://adsdiagnostic.netlify.app"
+    origins "http://localhost:5173", "https://adsdiagnostics.netlify.app"
 
     resource "*",
       headers: :any,
